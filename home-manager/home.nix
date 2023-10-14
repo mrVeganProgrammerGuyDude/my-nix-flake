7bbf4319a97f01370f4900bd6e3268844266ecd0
Sheldon Stewart <68789633+mrVeganProgrammerGuyDude@users.noreply.github.com>
2023-10-14 19:13:04 -0400

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,17 +1,21 @@
-{ config, pkgs, ... }:
-
 {
-
+  # Importing zsh.nix from the specified location
   imports = [
     ./apps/zsh.nix
   ];
+
+  # Setting up zsh (this line is commented out in the original code)
   # programs.zsh.enable = true;
-  home.username = "sheltronmini";
-  home.homeDirectory = "/home/sheltronmini";
-  home.stateVersion = "23.05";
+
+  # Configuring the home directory settings
+  home.username = "sheltronmini";         # Setting the username
+  home.homeDirectory = "/home/sheltronmini";  # Specifying the home directory path
+  home.stateVersion = "23.05";              # Setting the state version
+
+  # Installing packages using Nixpkgs
   home.packages = with pkgs; [
-    htop
-    whatsapp-for-linux
-    ledger-live-desktop
+    htop                        
+    whatsapp-for-linux          
+    ledger-live-desktop         
   ];
 }
